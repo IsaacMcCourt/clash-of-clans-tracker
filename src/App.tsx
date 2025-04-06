5369ffe156b83ce9a915b28112947962b31856c2
IsaacMcCourt <imccourtmagic@gmail.com>
2025-04-06 21:40:49 +0000

    Adding a dark mode toggle in the bottom right

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { Account } from './types';
 import { getAccounts } from './utils/storageUtils';
+import { ThemeProvider } from './contexts/ThemeContext';
 import './App.css';
 
-// We'll create these components next
+// Components
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import AccountDetail from './components/AccountDetail';
 import AccountForm from './components/AccountForm';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   const [accounts, setAccounts] = useState<Account[]>([]);
@@ -20,21 +22,24 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<Dashboard accounts={accounts} setAccounts={setAccounts} />} />
-            <Route path="/account/new" element={<AccountForm setAccounts={setAccounts} />} />
-            <Route path="/account/:id" element={<AccountDetail accounts={accounts} setAccounts={setAccounts} />} />
-          </Routes>
-        </main>
-        <footer className="footer">
-          <p>Clash of Clans Upgrade Tracker © {new Date().getFullYear()}</p>
-        </footer>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="app-container">
+          <Header />
+          <main className="content">
+            <Routes>
+              <Route path="/" element={<Dashboard accounts={accounts} setAccounts={setAccounts} />} />
+              <Route path="/account/new" element={<AccountForm setAccounts={setAccounts} />} />
+              <Route path="/account/:id" element={<AccountDetail accounts={accounts} setAccounts={setAccounts} />} />
+            </Routes>
+          </main>
+          <footer className="footer">
+            <p>Clash of Clans Upgrade Tracker © {new Date().getFullYear()}</p>
+          </footer>
+          <ThemeToggle />
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
